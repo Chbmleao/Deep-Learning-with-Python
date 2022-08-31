ce19a7563c52834724fb5af0366c567f413ef12e
Carlos Leão <caikleao@gmail.com>
2022-08-31 09:27:58 -0300

    Rename files

--- BaseBreastCancer/breast_cancer_simple.py
+++ BaseBreastCancer/breast_cancer_simple.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-predictors = pd.read_csv('entradas_breast.csv')
-rank = pd.read_csv('saidas_breast.csv')
+predictors = pd.read_csv('input_breast.csv')
+rank = pd.read_csv('output_breast.csv')
 
 from sklearn.model_selection import train_test_split
 trainingPredictors, testPredictors, trainingRank, testRank = train_test_split(predictors, rank, test_size=0.25)

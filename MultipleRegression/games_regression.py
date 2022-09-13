eb82b9a2666433c4f7b8e13340d43bc156a993e9
Carlos Leão <caikleao@gmail.com>
2022-09-13 15:10:15 -0300

    Add games_regression.py

---
+++ MultipleRegression/games_regression.py
@@ -0,0 +1,35 @@
+import pandas as pd
+from keras.layers import Dense, Dropout, Activation, Input
+from keras.models import Model
+
+database = pd.read_csv('games.csv')
+database = database.drop('Other_Sales', axis = 1)
+database = database.drop('Global_Sales', axis = 1)
+database = database.drop('Developer', axis = 1)
+
+database = database.dropna(axis = 0)
+database = database.loc[database['NA_Sales'] > 1]
+database = database.loc[database['EU_Sales'] > 1]
+
+database['Name'].value_counts()
+gamesName = database.Name
+database = database.drop('Name', axis = 1)
+
+predictors = database.iloc[:, [0, 1, 2, 3, 7, 8, 9, 10, 11]].values
+naSales = database.iloc[:, 4].values
+euSales = database.iloc[:, 5].values
+jpSales = database.iloc[:, 6].values
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder = LabelEncoder()
+predictors[:, 0] = labelEncoder.fit_transform(predictors[:, 0])
+predictors[:, 2] = labelEncoder.fit_transform(predictors[:, 2])
+predictors[:, 3] = labelEncoder.fit_transform(predictors[:, 3])
+predictors[:, 8] = labelEncoder.fit_transform(predictors[:, 8])
+
+from sklearn.compose import ColumnTransformer 
+ct = ColumnTransformer([("Name_Of_Your_Step", OneHotEncoder(),[0, 2, 3, 8])], remainder="passthrough")
+predictors = ct.fit_transform(predictors).toarray()    
+
+
+# neural network

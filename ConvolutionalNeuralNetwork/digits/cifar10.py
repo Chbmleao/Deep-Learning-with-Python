ce7567d9b007b20ca99ced9de71e7579ddc7c5e1
Carlos Leão <caikleao@gmail.com>
2023-03-12 19:28:11 -0300

    Finish cifar10 classifier

--- ConvolutionalNeuralNetwork/digits/cifar10.py
+++ ConvolutionalNeuralNetwork/digits/cifar10.py
@@ -13,5 +13,48 @@ assert x_test.shape == (10000, 32, 32, 3)
 assert y_train.shape == (50000, 1)
 assert y_test.shape == (10000, 1)
 
-
 plt.imshow(x_train[0])
+plt.title('Classe ' + str(y_train[0]))
+
+train_predictors = x_train.reshape(x_train.shape[0],
+                                   32, 32, 3)  
+test_predictors = x_test.reshape(x_test.shape[0],
+                                 32, 32, 3)
+train_predictors = train_predictors.astype('float32')
+test_predictors = test_predictors.astype('float32')
+
+test_predictors /= 255
+train_predictors /= 255
+
+train_class = np_utils.to_categorical(y_train, 10)
+test_class = np_utils.to_categorical(y_test, 10)
+
+classifier = Sequential()
+classifier.add(Conv2D(64, (3,3),
+                      input_shape=(32, 32, 3),
+                      activation = 'relu'))
+classifier.add(BatchNormalization())
+classifier.add(MaxPooling2D(pool_size = (2,2)))
+
+classifier.add(Conv2D(64, (3, 3), 
+                      activation='relu'))
+classifier.add(BatchNormalization())
+classifier.add(MaxPooling2D(pool_size = (2, 2)))
+classifier.add(Flatten())
+
+classifier.add(Dense(units = 256,
+                     activation = 'relu'))
+classifier.add(Dropout(0.2))
+classifier.add(Dense(units = 256, 
+                     activation = 'relu'))
+classifier.add(Dropout(0.2))
+classifier.add(Dense(units = 10,
+                     activation = 'softmax'))
+classifier.compile(loss = 'categorical_crossentropy',
+                   optimizer = 'adam',
+                   metrics = ['accuracy'])
+classifier.fit(train_predictors, train_class,
+               batch_size = 128, epochs = 5,
+               validation_data = (test_predictors, test_class))
+
+result = classifier.evaluate(test_predictors, test_class)

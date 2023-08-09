d183bb48aefc032690064fea031dc8048e92dec0
Carlos Leão <caikleao@gmail.com>
2023-08-09 16:30:57 -0300

    Add autoencoder

--- AutoEncoders/autoencoderDigits.py
+++ AutoEncoders/autoencoderDigits.py
@@ -12,7 +12,21 @@ trainPredictors = trainPredictors.reshape((len(trainPredictors), np.prod(trainPr
 
 testPredictors = testPredictors.reshape((len(testPredictors), np.prod(testPredictors.shape[1:])))
 
+# 784 - 32 - 784
+compressionFactor = 784 / 32
 
-
-
+autoencoder = Sequential()
+autoencoder.add(Dense(units=32, 
+                      activation="relu", 
+                      input_dim=784))
+autoencoder.add(Dense(units=784, activation="sigmoid"))
+autoencoder.summary()
+autoencoder.compile(optimizer="adam",
+                    loss="binary_crossentropy",
+                    metrics=["accuracy"])
+autoencoder.fit(trainPredictors, 
+                trainPredictors,
+                epochs=100,
+                batch_size=256,
+                validation_data=(testPredictors, testPredictors))
 

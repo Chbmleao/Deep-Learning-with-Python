e12bf806a87e99f9c144dc2b8336c55acd9b5c44
Carlos Leão <caikleao@gmail.com>
2023-08-10 15:25:16 -0300

    Add dummy classes and encoded predictors

--- AutoEncoders/autoencodeClassification.py
+++ AutoEncoders/autoencodeClassification.py
@@ -2,21 +2,24 @@ import numpy as np
 from keras.datasets import mnist
 from keras.models import Model, Sequential
 from keras.layers import Input, Dense
+from keras.utils import np_utils
 
-(trainPredictors, _), (testPredictors, _) = mnist.load_data()
+(trainPredictors, trainClass), (testPredictors, testClass) = mnist.load_data()
 trainPredictors = trainPredictors.astype("float32") / 255
 testPredictors = testPredictors.astype("float32") / 255
 
-trainPredictors = trainPredictors.reshape((len(trainPredictors), np.prod(trainPredictors.shape[1:])))
+trainDummyClass = np_utils.to_categorical(trainClass)
+testDummyClass = np_utils.to_categorical(testClass)
 
+trainPredictors = trainPredictors.reshape((len(trainPredictors), np.prod(trainPredictors.shape[1:])))
 testPredictors = testPredictors.reshape((len(testPredictors), np.prod(testPredictors.shape[1:])))
 
+# 784 - 32 - 784
 autoencoder = Sequential()
 autoencoder.add(Dense(units=32, 
                       activation="relu", 
                       input_dim=784))
 autoencoder.add(Dense(units=784, activation="sigmoid"))
-autoencoder.summary()
 autoencoder.compile(optimizer="adam",
                     loss="binary_crossentropy",
                     metrics=["accuracy"])
@@ -29,3 +32,6 @@ autoencoder.fit(trainPredictors,
 originalDimension = Input(shape=(784,))
 encoderLayer = autoencoder.layers[0]
 encoder = Model(originalDimension, encoderLayer(originalDimension))
+
+trainEncodedPredictors = encoder.predict(trainPredictors)
+testEncodedPredictors = encoder.predict(testPredictors)
